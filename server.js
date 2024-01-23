76e6a63a236a8fa04da814c43a378c95ea732261
bhumi <bhumi.rastogi05@gmail.com>
2024-01-23 15:53:21 +0530

    server file add

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = process.env.PUBLIC_PORT;
+
+// define the ping route
+app.get('/ping', (req,res)=>{
+    res.send("pong");
+})
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`🚀 server running on PORT: ${port}`);
+  });
+}
+
+module.exports = app;
