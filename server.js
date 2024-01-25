08279eaac6f34b3a8a49791ca4c8d8699a12b96b
bhumi <bhumi.rastogi05@gmail.com>
2024-01-25 16:19:35 +0530

    mongo

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+const { startDatabase, stopDatabase, isConnected } = require('./db');
+const app = express();
+
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'o_O',
+    database: isConnected() ? 'connected' : 'disconnected'
+  })
+});
+
+
+process.on('SIGINT', async () => {
+  await stopDatabase();
+  process.exit(0);
+});
+
+process.on('SIGTERM', async () => {
+  await stopDatabase();
+  process.exit(0);
+});
+
+if (require.main === module) {
+  app.listen(port, async () => {
+    await startDatabase();
+
+    console.log(`🚀 server running on PORT: $`);
+  });
+}
+
+
+
+
+
+module.exports = app;
